462101529e5a341abdc7aa0468a0de92549ce642
btcflip <b.elm0p2@gmail.com>
2016-10-30 00:09:54 +0200

    github

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LdKBAoUAAAAAOI4O57xyV3LPJ02sjvJxPOEL4_P',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  redirect_uri: 'http://cryptocases.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://socket.moneypot.com',
